9ff35ed543eb1909ec23901c35581c786be62918
Théo <63592879+TheoLomeuBraga@users.noreply.github.com>
2024-12-16 14:50:02 -0300

    a

--- Global_scripts/scene_manager.gd
+++ Global_scripts/scene_manager.gd
@@ -1,1 +1,26 @@
 extends Node
+
+var next_scene : String = ""
+var next_tag : String = ""
+
+func load_map(scene: String, tag: String):
+	next_scene = scene
+	ResourceLoader.load_threaded_request(scene)
+	next_tag = tag
+	
+
+func _process(delta: float) -> void:
+	
+	print("A")
+	$AspectRatioContainer.visible = next_scene != ""
+	if $AspectRatioContainer.visible:
+		var progress := []
+		ResourceLoader.load_threaded_get_status(next_scene,progress)
+		$AspectRatioContainer/VBoxContainer/ProgressBar.value = progress[0] * 100
+		
+		if progress[0] == 1:
+			get_tree().change_scene_to_packed(load(next_scene))
+			next_scene = ""
+			next_tag = ""
+			
+		
--- door/door.gd
+++ door/door.gd
@@ -1,5 +1,7 @@
 extends Area3D
 
+@export_file("*.tscn") var destination_scene : String = ""
+@export var destination_tag : String = ""
 
 func _on_body_entered(body: Node3D) -> void:
 	pass
