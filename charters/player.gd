54c60fea5e4ad4b6644d8b6720c2b19e499ba504
Théo <63592879+TheoLomeuBraga@users.noreply.github.com>
2024-12-16 14:28:33 -0300

    Initial commit

---
+++ charters/player.gd
@@ -0,0 +1,14 @@
+extends CharacterBody3D
+
+
+@export var speed : float = 512.0
+@export var turn_speed : float = 64.0
+
+#"ui_left", "ui_right", "ui_up", "ui_down"
+func _physics_process(delta: float) -> void:
+	
+	
+	rotation_degrees.y += Input.get_axis("ui_right","ui_left") * turn_speed * delta
+	
+	velocity = basis.z * Input.get_axis("ui_up", "ui_down") * speed * delta
+	move_and_slide()
