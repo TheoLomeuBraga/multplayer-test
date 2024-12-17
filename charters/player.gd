0a7c9c1777aea6671491b45a5f0776a28dc5128f
Théo <63592879+TheoLomeuBraga@users.noreply.github.com>
2024-12-17 11:20:47 -0300

    a

--- charters/player.gd
+++ charters/player.gd
@@ -4,8 +4,15 @@ class_name Player
 @export var speed : float = 512.0
 @export var turn_speed : float = 64.0
 
-#"ui_left", "ui_right", "ui_up", "ui_down"
+
+	
+
 func _physics_process(delta: float) -> void:
+	
+	if $RayCast3D.is_colliding():
+		var dist: float = $RayCast3D.global_position.distance_to( $RayCast3D.get_collision_point())
+		if dist > 0:
+			position.y -= dist
 	
 	rotation_degrees.y += Input.get_axis("ui_right","ui_left") * turn_speed * delta
 	
